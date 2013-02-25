bf612c4fc2f7f52ed78afaef82aef189cfb55538
Savvas Dalkitsis <savvas.dalkitsis@shazamteam.com>
2013-02-25 14:56:01 +0000

    Initial commit

---
+++ src/main/java/com/shazam/gwen/Assertion.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2013 Shazam Entertainment Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+*/
+package com.shazam.gwen;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * This is a marker annotation that declares that when a method is implemented, it is expected to perform a JUnit assertion.
+ */
+@Target(value = ElementType.METHOD)
+@Retention(value = RetentionPolicy.SOURCE)
+public @interface Assertion {}
