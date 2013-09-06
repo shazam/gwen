c44b193d1e4ddc8857b0d92008e640638766742b
Savvas Dalkitsis <savvas.dalkitsis@shazam.com>
2013-09-06 18:01:02 +0100

    Fix spacing

--- src/main/java/com/shazam/gwen/Gwen.java
+++ src/main/java/com/shazam/gwen/Gwen.java
@@ -18,28 +18,28 @@ import com.shazam.gwen.gwt.When;
 
 public class Gwen {
 
-	public static <T> T given(Given<T> object) {
-		return object.given();
-	}
+    public static <T> T given(Given<T> object) {
+        return object.given();
+    }
 
     public static <T extends Arranger> T given(T actor) {
         return actor;
     }
 
-	public static <T> T when(When<T> object) {
-		return object.when();
-	}
+    public static <T> T when(When<T> object) {
+        return object.when();
+    }
 
     public static <T extends Actor> T when(T actor) {
-   		return actor;
-   	}
+        return actor;
+    }
 
-	public static <T> T then(Then<T> object) {
-		return object.then();
-	}
+    public static <T> T then(Then<T> object) {
+        return object.then();
+    }
 
     public static <T extends Asserter> T then(T actor) {
-   		return actor;
-   	}
+        return actor;
+    }
 
 }
